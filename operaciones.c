c0499fe4baf2bfe070d3362722bd7674b59db54a
lopeznahuel45 <nahuellopez3711@gmail.com>
2026-05-04 01:32:33 -0300

    Laboratorio 2 Git-Github

--- operaciones.c
+++ operaciones.c
@@ -9,7 +9,7 @@ int sumar(int a, int b) {
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
 /* const asegura que los parámetros sean de solo lectura.*/
-int restar(int a, int b) {
+int restar(const int a, const int b) {
     return a - b;
 }
 
